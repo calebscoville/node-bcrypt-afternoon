312fded3581bf0c1d20e2f40a8c0555ceadac3fa
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-23 15:11:32 -0600

    fourth

---
+++ server/controllers/authController.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    register: async (req, res) => {
+        const { username, password, isAdmin } = req.body;
+        const db = req.app.get('db');
+        const result = await db.get_user([username]);
+        const existingUser = result[0];
+        if (existingUser) {
+            return res.status(409).send('Username taken');
+        }
+        const salt = bcrypt.gen
+    }
+}
