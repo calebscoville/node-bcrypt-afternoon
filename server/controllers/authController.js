7681e19efce9c41708e597642582c29814224c90
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-23 16:13:00 -0600

    ninth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,6 +13,21 @@ module.exports = {
         const hash = bcrypt.hashSync(password, salt);
         const registeredUser = await db.register_user([isAdmin, username, hash]);
         const user = registeredUser[0];
-        req.session.user = {}
-    }
-}
+        req.session.user = { isAdmin: user.is_admin, username: user.username, id: user.id };
+        return res.status(201).send(req.session.user);
+    },
+
+    login: async (req, res) => {
+        const { username, password } = req.body;
+        const foundUser = await req.app.get('db').get_user([username]);
+        const user = foundUser[0];
+        if (!user) {
+            return res.status(401).send('User not found. Please register as a new user before logging in.');
+        }
+        const isAuthenticated = bcrypt.compareSync(password, user.hash);
+        if (!isAuthenticated) {
+            return res.status(403).send('Incorrect password');
+        }
+        req.session.user = { isAdmin: user.is_admin, id: user.id, username: user.username };
+    },
+};
